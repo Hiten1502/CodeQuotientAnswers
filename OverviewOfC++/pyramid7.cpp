2514c5663cd798d3c8277901e3e70d07a6b26520
Hiten1502 <83130547+Hiten1502@users.noreply.github.com>
2021-07-03 17:05:32 +0530

    updated

--- OverviewOfC++/pyramid7.cpp
+++ OverviewOfC++/pyramid7.cpp
@@ -3,19 +3,16 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
+    int p =1;
     for (int i = 1; i <= n; i++)
     {
-        int p = i;
         for (int j = 1; j <= i; j++)
         {
-            cout<<p<<"";
-            p--;
-        }
-        int m = 2;
-        for (int x = 1; x < i; x++)
-        {
-            cout<<m<<"";
-            m++;
+            cout<<p;
+            if(j != i){
+                cout<<" ";
+            }
+            p++;
         }
         cout<<"\n";
     }
