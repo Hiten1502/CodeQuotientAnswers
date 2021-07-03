c746a18d25f54c9b04fbf3690a605f385da6f0e5
Hiten1502 <83130547+Hiten1502@users.noreply.github.com>
2021-07-03 14:23:21 +0530

    creating

---
+++ Functions/SumOfRange.cpp
@@ -0,0 +1,20 @@
+# include<bits/stdc++.h>
+using namespace std;
+
+int sumOfRange(int min, int max){
+  int sum(0);
+  for (int i = min; i <= max; i++)
+  {
+    sum += i;
+  }
+  return sum;
+}
+
+
+int main(){
+
+      
+    
+
+    return 0;
+}
