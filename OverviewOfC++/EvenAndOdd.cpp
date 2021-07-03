0e873eb8f9631e3f7d37b4ada8b273280a4074d8
Hiten1502 <83130547+Hiten1502@users.noreply.github.com>
2021-07-03 14:17:58 +0530

    Updated

--- OverviewOfC++/EvenAndOdd.cpp
+++ OverviewOfC++/EvenAndOdd.cpp
@@ -1,1 +1,13 @@
 
+# include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;cin>>n;
+    if(n%2 == 0){
+        cout<<"Even"<<"\n";
+    }
+    else{
+        cout<<"Odd"<<"\n";
+    }
+    return 0;
+}
