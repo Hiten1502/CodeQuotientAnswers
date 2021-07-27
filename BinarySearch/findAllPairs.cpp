f136280bff365743b2f87195668d6ad7f7efacb9
Hiten1502 <83130547+Hiten1502@users.noreply.github.com>
2021-07-27 10:08:13 +0530

    Update findAllPairs.cpp

--- BinarySearch/findAllPairs.cpp
+++ BinarySearch/findAllPairs.cpp
@@ -1,1 +1,13 @@
 
+int getPairsCount(int arr[], int n, int sum){
+   unordered_map<int, int> m;
+    for (int i = 0; i < n; i++)
+        m[arr[i]]++;
+    int c = 0;
+    for (int i = 0; i < n; i++) {
+        c += m[sum - arr[i]];
+        if (sum - arr[i] == arr[i])
+            c--;
+    }
+    return c / 2;
+}
