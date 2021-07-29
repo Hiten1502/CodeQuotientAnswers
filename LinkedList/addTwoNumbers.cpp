cf99ee27e262d293ef7703736a3746cb6b167991
Hiten1502 <83130547+Hiten1502@users.noreply.github.com>
2021-07-29 23:36:54 +0530

    Update addTwoNumbers.cpp

--- LinkedList/addTwoNumbers.cpp
+++ LinkedList/addTwoNumbers.cpp
@@ -1,12 +1,29 @@
-Node *ptr = NULL;
-void giveReverse(Node *head){
-  ptr = NULL;
-    if(head != NULL){
-      giveReverse(head->next);
-      insertEnd(&ptr,head->data);
+Node* addListNumbers(Node* head1,Node* head2){
+  	int x = 0;
+    int i = 0;
+    while (head1 != NULL)
+    {
+      int m = head1->data;
+      x = m*pow(10,i) + x;
+      i++;
+      head1 = head1->next;
     }
-}
-struct Node* reverseList(struct Node* head) {
-    giveReverse(head);
-  	return ptr;
+    int y = 0;
+    int j = 0;
+    while (head2 != NULL)
+    {
+      int m = head2->data;
+      y = m*pow(10,j) + y;
+      j++;
+      head2 = head2->next;
+    }
+    int sum = x+y;
+    Node *ptr = NULL;
+    while (sum > 0 )
+    {
+      int x = sum%10;
+      insertEnd(&ptr ,x);
+      sum = sum/10;
+    }
+    return ptr;
 }
