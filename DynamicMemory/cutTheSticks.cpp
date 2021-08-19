50941e7b4f1b2a84c18bc5e8960e7b9c072f7ce4
Hiten1502 <83130547+Hiten1502@users.noreply.github.com>
2021-08-19 12:03:20 +0530

    Update cutTheSticks.cpp

--- DynamicMemory/cutTheSticks.cpp
+++ DynamicMemory/cutTheSticks.cpp
@@ -1,3 +1,18 @@
+void sort(int arr[],int n){
+   for (int i = 0; i < n; ++i)
+    {
+        for (int j = i + 1; j < n; ++j)
+        {
+            if (arr[i] > arr[j])
+            {
+                int temp =  arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+}
+
 int* cutSticks(int lengths_size, int *lengths, int *result_size) {
    int i;
   	int *arr;
