4104e38277649a4398ac74a342513066ed7f94a4
Hiten1502 <83130547+Hiten1502@users.noreply.github.com>
2021-08-20 17:29:32 +0530

    Update implementQueueUsingLinkedList.cpp

--- Queue/implementQueueUsingLinkedList.cpp
+++ Queue/implementQueueUsingLinkedList.cpp
@@ -7,20 +7,31 @@ class Queue {
             next = NULL; 
         } 
     }; 
-
     QNode *front, *rear; 
-    
     public:
   		// Constructor
         Queue(){
-			
         }
   		// Add an element to the queue
         void enQueue(int x){
-			
+			QNode* newNode = new QNode(x);
+      if(rear == NULL){
+          front = rear = newNode;
+          return;
+      }
+      rear->next = newNode;
+      rear = newNode;
         }
   		// Delete front element from the queue
         int deQueue() { 
-			
+			 if(front == NULL){
+          return -1;
+       }
+        int temp = front->data;
+        front = front->next;
+        if (front == NULL){
+            rear = NULL;
+        }
+        return temp;
         }
 };
